7d5aeab47a4971bc971ad49f400f67e198ab492d
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-05-09 10:19:50 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main(){
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
   
   int t;
   int a, b;
@@ -12,7 +15,7 @@ int main(){
       int sum;
       std::cin >> a >> b;
       sum = a + b;
-      std::cout << "Case #" << i << ": " << sum << std::endl;
+      std::cout << "Case #" << i << ": " << sum << "\n";
     }
   return 0;
 }
