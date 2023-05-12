d249df61866244b022c9f27017ff122c870e8494
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-05-12 18:54:15 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+  int a;
+
+  std::cin >> a;
+
+for(int i = 0; i < a; i++)
+{
+  for(int j = 0; j > i -a; j--)
+    {
+      std::cout << "*";
+    }
+  std::cout << std::endl;
+}
+  return 0;
+}
