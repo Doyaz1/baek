669942073781e9b3a24090cb5eec3388459e2a54
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-05-19 13:30:00 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(){
+
+  int h, m, c;
+  int hour, minute;
+
+  std::cin >> h >> m;
+  std::cin >> c;
+
+  if(m + c < 60)
+  {
+    hour = h;
+    minute = m + c;
+  }
+  else
+  {
+    hour = (h + ((m + c) / 60)) % 24;
+    minute = (m + c) % 60;
+  }
+  std::cout << hour << " " << minute;
+  return 0;
+}
