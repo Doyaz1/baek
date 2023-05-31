c144b0eb07a51963679e62d0eef3ed6904aab3b0
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-05-31 22:09:59 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.cc
+++ 백준/Bronze/2562. 최댓값/최댓값.cc
@@ -4,15 +4,15 @@ int main(){
   // 배열 크기 설정
   int x[9];
   // 배열 값 설정
-  for(int i = 1; i <= 9; i++)
+  for(int i = 0; i < 9; i++)
   {
     std::cin >> x[i];    
   }
   // 쓰레기 값이 들어가지 않기 위한 최댓값, 순서값 변수 초기화
-  int max = x[0]; 
+  int max = -1; 
   int seq = 0; 
   // 설정 값을 토대로 비교하며 최댓값과 순서값 도출
-  for(int i = 1; i <= 9; i++){
+  for(int i = 0; i < 9; i++){
     if(max < x[i])
     {
       max = x[i];
@@ -20,6 +20,6 @@ int main(){
     }
   }
   std::cout << max << std::endl;
-  std::cout << seq << std::endl;
+  std::cout << seq +1 << std::endl;
   return 0;
 }
