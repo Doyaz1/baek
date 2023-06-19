c22b8f67d366685a0ffcf7b0dbbc5b1648f1f161
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-06-19 20:40:18 +0900

    [unrated] Title: 덧셈식 출력하기, Time: 2.21 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181947. 덧셈식 출력하기/덧셈식 출력하기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+    
+    int a, b;
+    int sum = 0;
+    
+    std::cin >> a >> b;
+    
+    sum = a + b;
+    
+    std::cout << a << " + " << b << " = " << sum;
+}
