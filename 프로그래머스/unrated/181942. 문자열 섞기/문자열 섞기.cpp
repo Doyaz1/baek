b74a967a0a8ca94785cf3ea6a6772e26777e32d3
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-06-22 22:45:36 +0900

    [unrated] Title: 문자열 섞기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+std::string solution(std::string str1, std::string str2) {
+    std::string result = "";
+    
+    for(int i = 0; i < str1.length(); i++){
+        result += str1[i];
+        result += str2[i];
+    }
+    return result;
+}
