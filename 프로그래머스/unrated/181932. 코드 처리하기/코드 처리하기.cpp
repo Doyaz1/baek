0fe718703edf53b590f186b523a28195bab3186f
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-06-27 22:08:29 +0900

    [unrated] Title: 코드 처리하기, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string answer = "";
+    int mode = 0;
+    
+    for(int idx = 0; idx < code.length(); idx++){
+        if(mode == 0){
+            if(code[idx] != '1'){
+                if(idx % 2 == 0){
+                    answer += code[idx];
+                }
+            }
+            else{
+                mode = 1;
+            }
+        }
+        else{
+            if(code[idx] != '1'){
+                if(idx % 2 == 1){
+                    answer += code[idx];
+                }
+            }
+            else{
+                mode = 0;
+            }
+        }
+    }
+    
+    if(answer.empty()){
+        return "EMPTY";
+    }
+    else{
+        return answer;
+    }
+    return answer;
+}
