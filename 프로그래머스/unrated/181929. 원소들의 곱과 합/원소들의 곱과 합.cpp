cc754cf91ccf236341304d018d9cc87d51688bc2
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-06-29 22:03:50 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int multi = 1;
+    int sum = 0;
+    
+    for(int i = 0; i < num_list.size(); i++){
+        multi *= num_list[i];
+        sum += num_list[i];
+    }
+    
+    if(multi < (sum * sum)){
+        answer = 1;
+    }
+    else{
+        answer = 0;
+    }
+    
+    return answer;
+}
