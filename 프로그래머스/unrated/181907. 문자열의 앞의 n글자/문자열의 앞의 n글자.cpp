788f36f10778bb9de8f432e32e2423b6e33952d1
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-07-08 22:49:18 +0900

    [unrated] Title: 문자열의 앞의 n글자, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cpp
+++ 프로그래머스/unrated/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cpp
@@ -5,10 +5,6 @@ using namespace std;
 
 string solution(string my_string, int n) {
     string answer = "";
-    
-    for(int i = 0; i < my_string.size(); i++){
-        answer = my_string.substr(0, n);
-    }
-    
-    return answer;
+
+    return answer = my_string.substr(0, n);
 }
