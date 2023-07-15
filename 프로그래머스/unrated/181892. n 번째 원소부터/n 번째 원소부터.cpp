d85beea2b6bb0ec6bd2cb198a454f6eeb7701a44
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-07-15 23:23:41 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    return answer = vector<int>(num_list.begin() + n - 1, num_list.end());
+}
