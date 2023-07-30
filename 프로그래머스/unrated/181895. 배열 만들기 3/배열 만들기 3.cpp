4c417765f19f0fefe9a906d6fba75b8c3389b7bd
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-07-30 22:01:46 +0900

    [unrated] Title: 배열 만들기 3, Time: 1.45 ms, Memory: 4.24 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> intervals) {
+    vector<int> answer;
+    // 첫번째 구간의 시작과 끝
+    int start1 = intervals[0][0];
+    int end1 = intervals[0][1];
+    // 두번째 구간의 시작과 끝
+    int start2 = intervals[1][0];
+    int end2 = intervals[1][1];
+    
+    for(int i = start1; i <= end1; i++){
+        answer.push_back(arr[i]);
+    }
+    
+    for(int i = start2; i <= end2; i++){
+        answer.push_back(arr[i]);
+    }
+    
+    return answer;
+}
