20f78308d654f5aeeb1d573423768b481a49c839
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-08-12 22:09:51 +0900

    [unrated] Title: 이차원 배열 대각선 순회하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181829. 이차원 배열 대각선 순회하기/이차원 배열 대각선 순회하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, int k) {
+    int answer = 0;
+    int sum = 0;
+    
+    for(int i = 0; i < board.size(); i++){
+        for(int j = 0; j < board[i].size(); j++){
+            if(i + j <= k){
+                sum += board[i][j];
+            }
+        }
+    }
+    
+    answer = sum;
+    
+    return answer;
+}
