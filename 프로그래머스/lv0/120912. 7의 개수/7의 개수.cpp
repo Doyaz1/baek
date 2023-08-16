6b216c48b21e3d98662e83f7f289f7c059d4323b
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-08-16 10:43:49 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.cpp
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.cpp
@@ -7,7 +7,7 @@ int solution(vector<int> array) {
     int answer = 0;
     
     for(int i = 0; i < array.size(); i++){
-        //10씩 나눴을 때 나머지가 7이 되면 +1, 0이 될때까지 반복
+        //10씩 나눴을 때 나머지가 7이 되면 +1, 0이 되기 전까지 반복
         while(array[i] != 0){
             if(array[i] % 10 == 7){
                 answer++;
