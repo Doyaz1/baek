2f229faf4752ea6c88746f74d15a43f8cb44fe56
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-08-22 21:51:10 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120909. 제곱수 판별하기/제곱수 판별하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 0; i < n; i++){
+        if( i * i == n){
+            return 1;
+        }
+    }
+    
+    return 2;
+}
