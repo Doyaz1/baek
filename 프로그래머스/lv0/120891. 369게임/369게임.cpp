4c08bf88e93865ecc8adba05d3d1bfd507e9e1f9
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-08-29 19:34:34 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    
+    while(order != 0){
+        if(order % 10 == 3){
+            answer++;
+        }
+        else if(order % 10 == 6){
+            answer++;
+        }
+        else if(order % 10 == 9){
+            answer++;
+        }
+        order /= 10;
+    }
+    
+    return answer;
+}
