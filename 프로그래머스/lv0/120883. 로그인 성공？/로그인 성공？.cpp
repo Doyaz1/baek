9cb4cffea8fb3094def63d086b70f39ac1ef9a6c
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-09-02 23:45:03 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db) {
+    string answer = "fail";
+    
+    for(int i = 0; i < db.size(); i++){
+        if(id_pw[0] == db[i][0] && id_pw[1] == db[i][1]){
+            return "login";
+        }
+        else if(id_pw[0] == db[i][0] && id_pw[1] != db[i][1]){
+            return "wrong pw";
+        }
+    }
+    
+    return answer;
+}
