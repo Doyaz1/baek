f0451e15c190a82d6c6e345d553f989d307ea688
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-09-04 21:12:26 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> spell, vector<string> dic) {
+    int answer = 2;
+    bool check = false;
+    
+    for(int i = 0; i < dic.size(); i++){
+        if(dic[i].size() == spell.size()){
+            check = true;
+            
+            for(int j = 0; j < spell.size(); j++){
+                if(dic[i].find(spell[j]) == string::npos){
+                    check = false;
+                    break;
+                }
+            }
+        }
+        if(check == true){
+            answer = 1;
+        }
+    }
+    
+    return answer;
+}
