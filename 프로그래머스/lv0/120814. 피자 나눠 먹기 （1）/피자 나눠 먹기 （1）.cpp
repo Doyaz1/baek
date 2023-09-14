c78d7e2c11de1ab44630c31ffd025bdd4fab456c
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-09-14 20:34:21 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    for(int i = 0; i < n; i++){
+        if(i % 7 == 0){
+            answer++;
+        }
+    }
+    
+    return answer;
+}
