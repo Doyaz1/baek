0e7ae394932b0a06ecb1935325367176a5a52fba
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-09-25 20:59:49 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    
+    for(auto a : my_string){
+        if(a >= '0' && a <= '9'){
+            answer.push_back(a - 48);
+        }
+    }
+    
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
