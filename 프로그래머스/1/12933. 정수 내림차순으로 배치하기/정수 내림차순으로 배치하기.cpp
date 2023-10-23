9d63328ebff72ad0350a73d93f1f0771e48c8d93
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-10-23 23:32:25 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    string number = to_string(n);
+    sort(number.rbegin(), number.rend());  // 내림 차순 정렬
+    
+    for(int i = 0; i < number.size(); i++){
+        answer = answer * 10 + (number[i] - '0');
+    }
+    
+    
+    return answer;
+}
