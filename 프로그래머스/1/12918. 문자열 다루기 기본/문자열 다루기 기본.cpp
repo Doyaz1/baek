3e592c69d2855de6b2b17ad88cbbb7f8ce47eb78
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2023-11-10 23:57:13 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+bool solution(string s) {
+    if(s.size() != 4 && s.size() != 6){
+        return false;
+    }
+    
+    for(char c : s){
+        if(isdigit(c) == false){
+            return false;
+        }
+    }
+    return true;
+}
