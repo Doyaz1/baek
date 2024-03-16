8424fe95e6d2e7ce185c337e3844f1952a4bd3e0
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2024-03-16 19:41:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int dp[1001];
+
+    cin >> a;
+
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+
+    for(int j = 4; j < 11; j++){
+        dp[j] = dp[j - 1] + dp[j - 2] + dp[j - 3];
+    }
+
+    for(int i = 0; i < a; i++){
+        cin >> b;
+
+        cout << dp[b] << '\n';
+    }
+
+    return 0;
+}
