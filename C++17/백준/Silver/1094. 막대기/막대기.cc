da85cd485e82293b644ce1e60e7256c55a683b8c
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2024-03-28 21:33:58 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int X, l = 64, cnt = 0, ret = 0, size = 0;
+    
+	cin >> X;
+    
+	while(1){
+		if (size == X or l == X) break; l /= 2;
+		if (size + l <= X) {
+			ret++;
+			size += l;
+		}
+	}
+	if (ret == 0){ 
+       cout << 1;
+    }
+	else cout << ret;
+}
