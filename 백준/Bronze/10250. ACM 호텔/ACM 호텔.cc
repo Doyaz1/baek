3c682ce05a6b3edd11c0bd6a9fe9527c4576ee83
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2025-04-27 02:39:17 +0900

    [Bronze III] Title: ACM 호텔, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -2,19 +2,21 @@
 
 int main() {
 	int t, h, w, n;
+	int floor;
 	int room;
 	std::cin >> t;
 
-	for (int i = 0; i < t; i++) {
+	while (t--) {
 		std::cin >> h >> w >> n;
 
-		if (n % h == 0) {
-			room = h * 100 + (n / h);
+		floor = n % h;
+		room = (n / h) + 1;
+
+		if (floor == 0) {
+			floor = h;
+			room = n / h;
 		}
-		else {
-			room = (n % h) * 100 + (n / h) + 1;
-		}
-		std::cout << room << std::endl;
+		std::cout << floor * 100 + room << std::endl;
 	}
 
 	return 0;
