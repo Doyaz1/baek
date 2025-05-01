cff7ebc1644e5b11062af5719d434704be93d0d4
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2025-05-01 10:24:19 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.cc
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -1,29 +1,26 @@
 #include <iostream>
 
-int main() 
-{
-  int score;
-  std::cin >> score;
-    
-  if(score >= 90)
-  {
-    std::cout << 'A';
-  }
-  else if(score >= 80)
-  {
-    std::cout << 'B';
-  }
-  else if(score >= 70)
-  {
-    std::cout << 'C';
-  }
-  else if(score >= 60)
-  {
-    std::cout << 'D';
-  }
-  else
-  {
-    std::cout << 'F';
-  }
-    return 0;
+int main() {
+	int score;
+	char grade;
+
+	std::cin >> score;
+
+	switch (score / 10) {
+	case 10: grade = 'A';
+			break;
+	case 9: grade = 'A';
+			break;
+	case 8: grade = 'B';
+			break;
+	case 7: grade = 'C';
+			break;
+	case 6: grade = 'D';
+			break;
+	default: grade = 'F';
+	}
+
+	std::cout << grade;
+
+	return 0;
 }
