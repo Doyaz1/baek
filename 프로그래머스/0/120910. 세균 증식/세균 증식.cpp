e338df83b99b0ef1ef1afb2f2686b50d3b7533d6
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2025-07-27 00:55:54 +0900

    [level 0] Title: 세균 증식, Time: 0.02 ms, Memory: 3.63 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int n, int t) {
+    int answer = 0;
+    int t1 = 0;
+    t1 = pow(2, t);
+    
+    answer = n * t1;
+    
+    
+    return answer;
+}
