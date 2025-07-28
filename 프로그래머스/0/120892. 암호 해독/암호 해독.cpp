3418d74d3fd8c4f7e413cf3942ec7a1b1b3b4f89
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2025-07-29 00:54:38 +0900

    [level 0] Title: 암호 해독, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+string solution(string cipher, int code) {
+    string answer = "";
+    
+    for(int i = 0; i < cipher.size(); i++){
+        if((i + 1) % code == 0){
+            answer += cipher[i];
+        }
+    }
+    
+    return answer;
+}
