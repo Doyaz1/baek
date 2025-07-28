ebced006239671c98874f998f44f521cbb171ef0
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2025-07-28 18:08:53 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 3.72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int money) {
+    vector<int> answer;
+    int americano = 5500;
+    
+    if(money % americano == 0){
+        answer.push_back(money / americano);
+        answer.push_back(0);
+    }
+    else if(money % americano != 0){
+        answer.push_back(money / americano);
+        answer.push_back(money - (americano * (money / americano)));
+    }
+    
+    
+    
+    return answer;
+}
