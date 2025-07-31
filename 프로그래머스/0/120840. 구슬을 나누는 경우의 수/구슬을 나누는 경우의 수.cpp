1ca45aa2cb946159ab2d896b8eb247cf79fab8cf
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2025-08-01 00:09:34 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int balls, int share) {
+    int answer = 1;
+    double a = 1;
+    
+    for(int i = 1; i <= share; i++){
+        a /= (double)i;
+        a *= (double)balls - i + 1;
+    }
+    
+    return (int)a;
+}
