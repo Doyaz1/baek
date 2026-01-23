3c8ce76bf4a06b19d030d19f24ec06a69c733e25
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2026-01-23 23:30:31 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+
+    cin >> x;
+
+    if (x < 6) {
+        cout << "Oh My God!";
+    }
+    else {
+        cout << "Success!";
+    }
+
+    return 0;
+}
