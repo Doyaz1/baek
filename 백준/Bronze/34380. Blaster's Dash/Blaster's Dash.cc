89427724a064208c4ec77f22ba0ab89ab3101466
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2026-02-02 23:54:01 +0900

    [Bronze IV] Title: Blaster's Dash, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34380. Blaster's Dash/Blaster's Dash.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int h, a;
+    int n;
+
+    cin >> h >> a;
+
+    if (h > a) {
+
+        cin >> n;
+    }
+
+    cout << (10 + 10 + n) * 2;
+
+
+    return 0;
+}
