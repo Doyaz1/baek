2d599f5db5150f5be67d5cfa263806895c5a4c6d
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2026-03-08 23:37:55 +0900

    [Bronze III] Title: And Then There Was 5, Time: 264 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11520. And Then There Was 5/And Then There Was 5.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int t;
+
+	cin >> t;
+
+	for (int i = 0; i < t; i++) {
+		int Q, D;
+
+		cin >> Q >> D;
+
+		cout << D << " " << 5 << endl;
+	}
+	
+
+	return 0;
+}
