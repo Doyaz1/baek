d74759923910089c44f0acf319b9b0cbede99dde
Doyaz1 <131674386+Doyaz1@users.noreply.github.com>
2026-03-10 23:42:31 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int x, y, z;
+
+	cin >> x >> y >> z;
+
+	int mul = x * y;
+	int sum = mul + z;
+
+	cout << sum << endl;
+
+	return 0;
+}
